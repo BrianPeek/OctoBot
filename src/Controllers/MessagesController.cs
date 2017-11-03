749e7be9a02c0fda5cdb7c7073fd36f0f6a1372e
Brian Peek <brian@brianpeek.com>
2017-11-02 21:06:18 -0400

    Update intro text to signify it's a demo.

--- src/Controllers/MessagesController.cs
+++ src/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net;
@@ -46,14 +47,21 @@ namespace GitHubBot
 							IEnumerable<ChannelAccount> newMembers = update.MembersAdded?.Where(t => t.Id != activity.Recipient.Id);
 							foreach (var newMember in newMembers)
 							{
-								reply.Text = $"Welcome {newMember.Name}! I can help you with getting information about your GitHub repos. ";
+								reply.Text = "The **[GitHub Information Bot](https://docs.microsoft.com/en-us/sandbox/demos/githubbot)** is a sample application that demonstrates how to use " +
+								             "[LUIS.ai](https://luis.ai/), [Bot Framework](https://dev.botframework.com/), and [Octokit.NET](https://github.com/octokit/octokit.net) to interact " +
+								             "with your GitHub account, repos, issues, etc. from within a chat window. " +
+								             "For more information on how it was made, please see its article on [The Sandbox](https://docs.microsoft.com/en-us/sandbox/demos/githubbot). " +
+								             "Please note that this is strictly a **demo**, not a supported product, and is subject to [GitHub's rate limiting policy](https://developer.github.com/v3/#rate-limiting). " +
+								             "Enjoy!" + Environment.NewLine + Environment.NewLine + "---" + Environment.NewLine + Environment.NewLine;
 
-								IBotData data = scope.Resolve<IBotData>();
+								reply.Text += $"Welcome {newMember.Name}! I can help you with getting information about your GitHub repos.";
+
+								IBotData data = scope.Resolve<IBotData>();	
 								await data.LoadAsync(CancellationToken.None);
 								if(data.UserData.ContainsKey(Constants.AuthTokenKey))
-									reply.Text += "It looks like you're already logged in, so what can I help you with?";
+									reply.Text += " It looks like you're already logged in, so what can I help you with?";
 								else
-									reply.Text += "To get started, type **login** to authorize me to talk to GitHub on your behalf, or type **help** to get more information.";
+									reply.Text += " To get started, type **login** to authorize me to talk to GitHub on your behalf, or type **help** to get more information.";
 
 								await client.Conversations.ReplyToActivityAsync(reply);
 							}
